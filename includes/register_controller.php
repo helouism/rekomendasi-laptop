bfba6c2d5bcc75c47d74933ff6ff81d2b5697c5b
Hendrik Louis Mahdi <hendrikmahdi@gmail.com>
2024-08-08 22:49:06 +0700

    Update register_controller.php
    Add CSRF token validation

--- includes/register_controller.php
+++ includes/register_controller.php
@@ -2,6 +2,11 @@
 include($_SERVER['DOCUMENT_ROOT'] . '/rekomendasi-laptop/config/config.php');
 session_start();
 
+if (empty($_SESSION['token'])) {
+    $_SESSION['token'] = bin2hex(random_bytes(32));
+}
+$token = $_SESSION['token'];
+
 // Jika user sudah login, arahkan ke dashboard
 if (isset($_SESSION['username'])) {
     header("Location: dashboard");
@@ -27,36 +32,43 @@ function containsSpace($string)
 
 if (isset($_POST['submit'])) {
 
-    $username = isset($_POST['username']) ? htmlspecialchars($_POST['username'], ENT_QUOTES, 'UTF-8') : '';
-    $password = $_POST['password'];
-    $cpassword = $_POST['cpassword'];
+    if (!empty($_POST['token'])) {
+        if (hash_equals($_SESSION['token'], $_POST['token'])) {
+            $username = isset($_POST['username']) ? htmlspecialchars($_POST['username'], ENT_QUOTES, 'UTF-8') : '';
+            $password = $_POST['password'];
+            $cpassword = $_POST['cpassword'];
 
-    if (containsSpace($username)) {
-        $warning = 'Username tidak boleh mengandung spasi.';
-    } elseif (!isPasswordValid($password)) {
-        $warning = 'Password harus mengandung setidaknya satu angka, satu huruf besar, satu huruf kecil, dan minimal 8 karakter.';
-    } elseif ($password == $cpassword) {
-        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+            if (containsSpace($username)) {
+                $warning = 'Username tidak boleh mengandung spasi.';
+            } elseif (!isPasswordValid($password)) {
+                $warning = 'Password harus mengandung setidaknya satu angka, satu huruf besar, satu huruf kecil, dan minimal 8 karakter.';
+            } elseif ($password == $cpassword) {
+                $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-        $stmt = $conn->prepare("SELECT * FROM tb_user WHERE username = ?");
-        $stmt->bind_param("s", $username);
-        $stmt->execute();
-        $result = $stmt->get_result();
+                $stmt = $conn->prepare("SELECT * FROM tb_user WHERE username = ?");
+                $stmt->bind_param("s", $username);
+                $stmt->execute();
+                $result = $stmt->get_result();
 
-        if ($result->num_rows == 0) {
-            $stmt = $conn->prepare("INSERT INTO tb_user (username, password) VALUES (?, ?)");
-            $stmt->bind_param("ss", $username, $hashed_password);
-            if ($stmt->execute()) {
+                if ($result->num_rows == 0) {
+                    $stmt = $conn->prepare("INSERT INTO tb_user (username, password) VALUES (?, ?)");
+                    $stmt->bind_param("ss", $username, $hashed_password);
+                    if ($stmt->execute()) {
 
-                $username = "";
-                header("Location: login");
+                        $username = "";
+                        header("Location: login");
+                    } else {
+                        $warning = 'Maaf Terjadi kesalahan.';
+                    }
+                } else {
+                    $warning = 'Maaf Username Sudah Terdaftar.';
+                }
             } else {
-                $warning = 'Maaf Terjadi kesalahan.';
+                $warning = 'Password Tidak Sesuai';
             }
         } else {
-            $warning = 'Maaf Username Sudah Terdaftar.';
+            error_log("CSRF token validation failed, IP Address: " . $_SERVER['REMOTE_ADDR']);
+            die("token validation failed");
         }
-    } else {
-        $warning = 'Password Tidak Sesuai';
     }
 }
